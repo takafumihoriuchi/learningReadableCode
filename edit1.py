ecaf2643adb404c40c58357abdede937d2b9c441
Takafumi Horiuchi <hawkletter080e@icloud.com>
2017-11-20 15:17:59 +0900

    modified to meet flask8 style

--- edit1.py
+++ edit1.py
@@ -1,23 +1,21 @@
-"""
-change loop index to something more recognizable than 'i's or 'j's
-"""
-
 import sys
+
 
 class Board(object):
     board = []
+
     def __init__(self, row, col, blank):
         self.row = row
         self.col = col
         self.blank = blank
-  
+
     def createBoard(self):
         for i in range(self.row):
-          self.board.append([self.blank]*self.col)
+            self.board.append([self.blank] * self.col)
 
     def setBoard(self, set_row, set_col, color):
         self.board[set_row][set_col] = color
-  
+
     def printBoard(self):
         print ""
         for i in self.board:
@@ -25,29 +23,29 @@ class Board(object):
         print ""
 
     def possibleChoice(self, color):
-        possibility = 0 #number of choices of places where possible to put stone
+        possibility = 0
         for ri in range(self.row):
             for ci in range(self.col):
-                if self.board[ri][ci]==self.blank:
-                    if self.countUp(ri,ci,color)>0:
-                        possibility+=1
+                if self.board[ri][ci] == self.blank:
+                    if self.countUp(ri, ci, color) > 0:
+                        possibility += 1
         return possibility
 
-    def gameSet(self, player_color, ai_color): #print out results
+    def gameSet(self, player_color, ai_color):  # print out results
         print("------------------")
         cnt_player = 0
         cnt_ai = 0
         for ri in range(self.row):
             for ci in range(self.col):
-                if self.board[ri][ci]==player_color:
-                    cnt_player+=1
-                elif self.board[ri][ci]==ai_color:
-                    cnt_ai+=1
+                if self.board[ri][ci] == player_color:
+                    cnt_player += 1
+                elif self.board[ri][ci] == ai_color:
+                    cnt_ai += 1
         print("Player (Black): %d" % cnt_player)
         print("AI     (White): %d" % cnt_ai)
-        if cnt_player>cnt_ai:
+        if cnt_player > cnt_ai:
             print("Winner        : Player")
-        elif cnt_player<cnt_ai:
+        elif cnt_player < cnt_ai:
             print("Winner        : AI")
         else:
             print("Draw")
@@ -55,74 +53,74 @@ class Board(object):
         sys.exit()
 
     def countUp(self, row, col, color):
-        count=0
-        #down
-        for i in range(row+1, self.row):
-            if self.board[i][col]==color:
-                for ii in range(row+1, i):
-                    count+=1
+        count = 0
+        # down
+        for i in range(row + 1, self.row):
+            if self.board[i][col] == color:
+                for ii in range(row + 1, i):
+                    count += 1
                 break
-            elif self.board[i][col]==self.blank:
+            elif self.board[i][col] == self.blank:
                 break
-        #right
-        for i in range(col+1, self.col):
-            if self.board[row][i]==color:
-                for ii in range(col+1, i):
-                    count+=1
+        # right
+        for i in range(col + 1, self.col):
+            if self.board[row][i] == color:
+                for ii in range(col + 1, i):
+                    count += 1
                 break
-            elif self.board[row][i]==self.blank:
+            elif self.board[row][i] == self.blank:
                 break
-        #up
-        for i in range(row-1,-1,-1):
-            if self.board[i][col]==color:
-                for ii in range(row-1,i,-1):
-                    count+=1
+        # up
+        for i in range(row - 1, -1, -1):
+            if self.board[i][col] == color:
+                for ii in range(row-1, i, -1):
+                    count += 1
                 break
-            elif self.board[i][col]==self.blank:
+            elif self.board[i][col] == self.blank:
                 break
-        #left
-        for i in range(col-1,-1,-1):
-            if self.board[row][i]==color:
-                for ii in range(col-1,i,-1):
-                    count+=1
+        # left
+        for i in range(col - 1, -1, -1):
+            if self.board[row][i] == color:
+                for ii in range(col - 1, i, -1):
+                    count += 1
                 break
-            elif self.board[row][i]==self.blank:
+            elif self.board[row][i] == self.blank:
                 break
-        #left-upper
+        # left-upper
         limit = min(row, col)
-        for i in range(1,limit+1):
-            if self.board[row-i][col-i]==color:
-                for ii in range(1,i):
-                    count+=1
+        for i in range(1, limit + 1):
+            if self.board[row - i][col - i] == color:
+                for ii in range(1, i):
+                    count += 1
                 break
-            elif self.board[row-1][col-1]==self.blank:
+            elif self.board[row - 1][col - 1] == self.blank:
                 break
-        #right-lower
-        limit = min(self.row-row, self.col-col)
-        for i in range(1,limit):
-            if self.board[row+i][col+i]==color:
-                for ii in range(1,i):
-                    count+=1
+        # right-lower
+        limit = min(self.row - row, self.col - col)
+        for i in range(1, limit):
+            if self.board[row + i][col + i] == color:
+                for ii in range(1, i):
+                    count += 1
                 break
-            elif self.board[row+i][col+i]==self.blank:
+            elif self.board[row + i][col + i] == self.blank:
                 break
-        #left-lower
-        limit = min(self.row-row, col+1)
-        for i in range(1,limit):
-            if self.board[row+i][col-i]==color:
-                for ii in range(1,i):
-                    count+=1
+        # left-lower
+        limit = min(self.row - row, col + 1)
+        for i in range(1, limit):
+            if self.board[row + i][col - i] == color:
+                for ii in range(1, i):
+                    count += 1
                 break
-            elif self.board[row+i][col-i]==self.blank:
+            elif self.board[row + i][col - i] == self.blank:
                 break
-        #right-upper
-        limit = min(row+1, self.col-col)
-        for i in range(1,limit):
-            if self.board[row-i][col+i]==color:
-                for ii in range(1,i):
-                    count+=1
+        # right-upper
+        limit = min(row + 1, self.col - col)
+        for i in range(1, limit):
+            if self.board[row - i][col + i] == color:
+                for ii in range(1, i):
+                    count += 1
                 break
-            elif self.board[row-i][col+i]==self.blank:
+            elif self.board[row - i][col + i] == self.blank:
                 break
         return count
 
@@ -138,11 +136,12 @@ class Player(object):
         self.putStone()
 
     def putStone(self):
-        if self.row not in range(self.my_board.row) or self.col not in range(self.my_board.col):
+        if self.row not in range(self.my_board.row)\
+         or self.col not in range(self.my_board.col):
             print("invalid input (out of range)")
             self.selectPoint()
-        elif self.my_board.board[self.row][self.col]==my_board.blank:
-            if my_board.countUp(self.row, self.col, self.color)>0:
+        elif self.my_board.board[self.row][self.col] == my_board.blank:
+            if my_board.countUp(self.row, self.col, self.color) > 0:
                 self.my_board.board[self.row][self.col] = self.color
             else:
                 print("invalid input (unflippable)")
@@ -153,108 +152,109 @@ class Player(object):
         self.flipStone()
 
     def flipStone(self):
-        #down
-        for i in range(self.row+1, my_board.row): #this will not be executed if self.row is out of range
-            if my_board.board[i][self.col]==self.color:
-                for ii in range(self.row+1, i):
+        # down
+        for i in range(self.row + 1, my_board.row):
+            if my_board.board[i][self.col] == self.color:
+                for ii in range(self.row + 1, i):
                     my_board.board[ii][self.col] = self.color
                 break
-            elif my_board.board[i][self.col]==my_board.blank:
+            elif my_board.board[i][self.col] == my_board.blank:
                 break
-        #right
-        for i in range(self.col+1, my_board.col):
-            if my_board.board[self.row][i]==self.color:
-                for ii in range(self.col+1, i):
+        # right
+        for i in range(self.col + 1, my_board.col):
+            if my_board.board[self.row][i] == self.color:
+                for ii in range(self.col + 1, i):
                     my_board.board[self.row][ii] = self.color
                 break
-            elif my_board.board[self.row][i]==my_board.blank:
+            elif my_board.board[self.row][i] == my_board.blank:
                 break
-        #up
-        for i in range(self.row-1,-1,-1):
-            if my_board.board[i][self.col]==self.color:
-                for ii in range(self.row-1,i,-1):
+        # up
+        for i in range(self.row - 1, -1, -1):
+            if my_board.board[i][self.col] == self.color:
+                for ii in range(self.row - 1, i, -1):
                     my_board.board[ii][self.col] = self.color
                 break
-            elif my_board.board[i][self.col]==my_board.blank:
+            elif my_board.board[i][self.col] == my_board.blank:
                 break
-        #left
-        for i in range(self.col-1,-1,-1):
-            if my_board.board[self.row][i]==self.color:
-                for ii in range(self.col-1,i,-1):
+        # left
+        for i in range(self.col - 1, -1, -1):
+            if my_board.board[self.row][i] == self.color:
+                for ii in range(self.col - 1, i, -1):
                     my_board.board[self.row][ii] = self.color
                 break
-            elif my_board.board[self.row][i]==my_board.blank:
+            elif my_board.board[self.row][i] == my_board.blank:
                 break
-        #left-upper
+        # left-upper
         limit = min(self.row, self.col)
-        for i in range(1,limit+1):
-            if my_board.board[self.row-i][self.col-i]==self.color:
-                for ii in range(1,i):
-                    my_board.board[self.row-ii][self.col-ii] = self.color
+        for i in range(1, limit + 1):
+            if my_board.board[self.row - i][self.col - i] == self.color:
+                for ii in range(1, i):
+                    my_board.board[self.row - ii][self.col - ii] = self.color
                 break
-            elif my_board.board[self.row-1][self.col-1]==my_board.blank:
+            elif my_board.board[self.row - 1][self.col - 1] == my_board.blank:
                 break
-        #right-lower
-        limit = min(my_board.row-self.row, my_board.col-self.col)
-        for i in range(1,limit):
-            if my_board.board[self.row+i][self.col+i]==self.color:
-                for ii in range(1,i):
-                    my_board.board[self.row+ii][self.col+ii] = self.color
+        # right-lower
+        limit = min(my_board.row - self.row, my_board.col - self.col)
+        for i in range(1, limit):
+            if my_board.board[self.row + i][self.col + i] == self.color:
+                for ii in range(1, i):
+                    my_board.board[self.row + ii][self.col + ii] = self.color
                 break
-            elif my_board.board[self.row+i][self.col+i]==my_board.blank:
+            elif my_board.board[self.row + i][self.col + i] == my_board.blank:
                 break
-        #left-lower
-        limit = min(my_board.row-self.row, self.col+1)
-        for i in range(1,limit):
-            if my_board.board[self.row+i][self.col-i]==self.color:
-                for ii in range(1,i):
-                    my_board.board[self.row+ii][self.col-ii] = self.color
+        # left-lower
+        limit = min(my_board.row - self.row, self.col + 1)
+        for i in range(1, limit):
+            if my_board.board[self.row + i][self.col - i] == self.color:
+                for ii in range(1, i):
+                    my_board.board[self.row + ii][self.col - ii] = self.color
                 break
-            elif my_board.board[self.row+i][self.col-i]==my_board.blank:
+            elif my_board.board[self.row + i][self.col - i] == my_board.blank:
                 break
-        #right-upper
-        limit = min(self.row+1, my_board.col-self.col)
-        for i in range(1,limit):
-            if my_board.board[self.row-i][self.col+i]==self.color:
-                for ii in range(1,i):
-                    my_board.board[self.row-ii][self.col+ii] = self.color
+        # right-upper
+        limit = min(self.row + 1, my_board.col - self.col)
+        for i in range(1, limit):
+            if my_board.board[self.row - i][self.col + i] == self.color:
+                for ii in range(1, i):
+                    my_board.board[self.row - ii][self.col + ii] = self.color
                 break
-            elif my_board.board[self.row-i][self.col+i]==my_board.blank:
+            elif my_board.board[self.row - i][self.col + i] == my_board.blank:
                 break
 
 
 class ArtificialIntelligence(Player):
     def aiCalculate(self):
-        #check the four-corners first
+        # check the four-corners first
         max_count = 0
         for i in range(2):
             for j in range(2):
-                if my_board.board[i*(my_board.row-1)][j*(my_board.col-1)]==my_board.blank:
-                    count = my_board.countUp(i*(my_board.row-1),j*(my_board.col-1),self.color)
-                    if count>max_count:
+                if my_board.board[i * (my_board.row - 1)][j * (my_board.col - 1)] == my_board.blank:
+                    count = my_board.countUp(i * (my_board.row - 1), j * (my_board.col - 1), self.color)
+                    if count > max_count:
                         max_count = count
-                        self.row = i*(my_board.row-1)
-                        self.col = j*(my_board.col-1)
-        if max_count>0:
+                        self.row = i * (my_board.row - 1)
+                        self.col = j * (my_board.col - 1)
+        if max_count > 0:
             self.putStone()
         else:
-            #if corner not possible, select point to get highest return in that round (if multiple choice, select point that appears first)
+            # select point to get highest return in that round
+            # (select point that appears first)
             max_count = 0
             for i in range(my_board.row):
                 for j in range(my_board.col):
-                    if my_board.board[i][j]==my_board.blank:
-                        count = my_board.countUp(i,j,self.color)
-                        if count>max_count:
+                    if my_board.board[i][j] == my_board.blank:
+                        count = my_board.countUp(i, j, self.color)
+                        if count > max_count:
                             max_count = count
                             self.row = i
                             self.col = j
-            if max_count>0:
+            if max_count > 0:
                 self.putStone()
 
 
-my_board = Board(8, 8, "O")  # initialize board with "O" to represent open space
-my_player = Player("B", my_board)  # set my_player.color to "B" to represent black
-my_ai = ArtificialIntelligence("W", my_board)  # set my_ai.color to "W" to represent white
+my_board = Board(8, 8, "O")  # initialize board with "O" : open space
+my_player = Player("B", my_board)  # set my_player.color to "B" : black
+my_ai = ArtificialIntelligence("W", my_board)  # set my_ai.color to "W" : white
 my_board.createBoard()
 my_board.setBoard(4, 3, my_player.color)
 my_board.setBoard(3, 4, my_player.color)
@@ -274,3 +274,8 @@ while True:
     else:
         if not my_board.possibleChoice(my_player.color) > 0:
             my_board.gameSet(my_player.color, my_ai.color)
+
+
+"""
+change loop index to something more recognizable than 'i's or 'j's
+"""
