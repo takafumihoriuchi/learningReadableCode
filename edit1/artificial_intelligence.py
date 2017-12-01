abd03ede40d33ccf6380088453423c8baf731c2b
Takafumi Horiuchi <hawkletter080e@icloud.com>
2017-12-01 21:28:36 +0900

    deleted un-needed comments

--- edit1/artificial_intelligence.py
+++ edit1/artificial_intelligence.py
@@ -20,22 +20,6 @@ class ArtificialIntelligence(Player):
                 self.col = j * (self.my_board.col - 1)
         if max_count > 0:
             self.put_stone()
-
-        """
-        for i in range(2):  # （「ここはネストが深いので修正できる」＝＞検討中）
-            for j in range(2):
-                # もし角が空いていれば
-                if self.my_board.board[i * (self.my_board.row - 1)][j * (self.my_board.col - 1)] == self.my_board.blank:  # noqa
-                    # そこに置くことが可能かどうかを確かめて
-                    count = self.my_board.count_up(i * (self.my_board.row - 1), j * (self.my_board.col - 1), self.color)  # noqa
-                    # もし置くことが可能であれば、それらのうち最大の個数を保持する
-                    if count > max_count:
-                        max_count = count
-                        self.row = i * (self.my_board.row - 1)
-                        self.col = j * (self.my_board.col - 1)
-        if max_count > 0:
-            self.put_stone()
-        """
         else:
             # select to get highest return in that turn
             max_count = 0
